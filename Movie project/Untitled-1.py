bb252816b27175c77580307be5b015e32fb81ae9
Sakshams2008 <saksham.sharma@npbhs.school.nz>
2024-08-20 12:07:07 +1200

    Movie table and add functionality
    Done with creating the movie table and started with the add movie functionality.

--- Movie project/Untitled-1.py
+++ Movie project/Untitled-1.py
@@ -1,1 +1,25 @@
-adrh
+import sqlite3
+import easygui as e
+
+# Connect to SQLite database.
+conn = sqlite3.connect("movie database.db")
+cursor = conn.cursor()
+
+# Create the movie table
+cursor.execute('''
+    CREATE TABLE Movie (
+        ID INTEGER PRIMARY KEY,
+        Title TEXT NOT NULL,
+        Year DATE NOT NULL,
+        Rating TEXT NOT NULL,
+        Duration INTEGER NOT NULL
+    )
+''')
+conn.commit()
+
+# Input add movie functionality.
+def add_movie():
+    title = e.enterbox("Enter the movie title:", "Add movie")
+    if not title:
+        e.msgbox("Title required!", "Error")
+        return
